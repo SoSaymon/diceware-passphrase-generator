33621e42c4de37405b5d9d5f85214e866d988e09
SoSaymon <szymon.chirowski@protonmail.com>
2023-06-08 14:56:27 +0200

    Add wordlist maker into wordlist_loader.py

--- wordlist_loader.py
+++ wordlist_loader.py
@@ -21,10 +21,8 @@ def make_dict(line):
     return word_dict
 
 
-if __name__ == "__main__":
-    wordlist = load_wordlist('wordlist.txt')
-
+def make_wordlist(wordlist):
     dict_list = []
     for i in range(len(wordlist)):
         dict_list.append(make_dict(wordlist[i]))
-    print(dict_list)
+    return dict_list
