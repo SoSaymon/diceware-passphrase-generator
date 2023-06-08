b07056f89b81211e8716a2cde8a964210f7c1e91
SoSaymon <szymon.chirowski@protonmail.com>
2023-06-08 20:52:55 +0200

    Add one-liner

--- dice.py
+++ dice.py
@@ -6,9 +6,7 @@ def generate_numbers(number_of_numbers=5) -> list[int]:
     num_arr = []
     i = 0
     while i < number_of_numbers:
-        num = random.random()
-        num *= 10
-        num = round(num)
+        num = round(random.random() * 10)
         if num > 6 or num < 1:
             continue
         num_arr.append(num)
@@ -20,7 +18,7 @@ def concat_all_numbers(num_array: list[int]) -> str:
     return ''.join(str(i) for i in num_array)
 
 
-def main(number_of_numbers=5):
+def main(number_of_numbers=5) -> str:
     return concat_all_numbers(generate_numbers(number_of_numbers))
 
 
