f7613f5c821ce3158039452a1e12fc808000fe2f
SoSaymon <szymon.chirowski@protonmail.com>
2023-06-09 21:46:54 +0200

    Upgrade tests

--- tests/test_passgen.py
+++ tests/test_passgen.py
@@ -1,20 +1,23 @@
 import sys
 
+import pytest
+
 sys.path.append("/home/sosaymon/Projects/diceware-password-generator/")
 
 from passgen import find_index, get_word
 
+test_inputs = ["1", "2", "3", "4"]
+expected_values_test_find_index = [0, 1, 2, -1]
 
-def test_find_index():
+
+@pytest.mark.parametrize("test_input,expected", zip(test_inputs, expected_values_test_find_index))
+def test_find_index(test_input: str, expected: int):
     wordlist = [
         {"number": "1", "word": "a"},
         {"number": "2", "word": "b"},
         {"number": "3", "word": "c"},
     ]
-    assert find_index(wordlist, "1") == 0
-    assert find_index(wordlist, "2") == 1
-    assert find_index(wordlist, "3") == 2
-    assert find_index(wordlist, "4") == -1
+    assert find_index(wordlist, test_input) == expected
 
 
 def test_get_word():
