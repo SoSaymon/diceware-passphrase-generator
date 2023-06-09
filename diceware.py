67147ea26d97c83fcf66cdc19dafa263a53cf41f
SoSaymon <szymon.chirowski@protonmail.com>
2023-06-09 17:24:41 +0200

    Change types and function name in diceware.py

--- diceware.py
+++ diceware.py
@@ -1,8 +1,11 @@
 import passgen
 import argparse
 
+PassphraseWordLength = int
+AvailableWords = str
 
-def interactive_cli() -> (int, str):
+
+def get_user_options() -> (PassphraseWordLength, AvailableWords):
     print("Welcome in diceware generation program")
     number_of_words = int(input("How many words should the passphrase contain?\n"))
     wordlist = input("What wordlist do you want to use? Leave blank if you want to use the default one.")
@@ -12,8 +15,8 @@ def interactive_cli() -> (int, str):
 
 def main():
     parser = argparse.ArgumentParser(prog="diceware.py",
-                                    usage="python3 %(prog)s [-h] [-w NUMBER OF WORDS] [--wordlist=wordlist.txt]",
-                                    description="Passphrase generator based on diceware method")
+                                     usage="python3 %(prog)s [-h] [-w NUMBER OF WORDS] [--wordlist=wordlist.txt]",
+                                     description="Passphrase generator based on diceware method")
     parser.add_argument("-w", "--words", type=int, help="Number of words in passphrase")
     parser.add_argument("--wordlist", type=str, default="wordlist.txt", help="Wordlist used to generate the passphrase")
 
@@ -22,7 +25,7 @@ def main():
     wordlist = args.wordlist
 
     if number_of_words is None:
-        number_of_words, wordlist = interactive_cli()
+        number_of_words, wordlist = get_user_options()
         if wordlist == "":
             wordlist = "wordlist.txt"
 
